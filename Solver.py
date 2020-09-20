6589d944ad09e0a2976af642dea5a4b27da61014
Jorge1994 <jorgesilva.fcp.1994@gmail.com>
2020-09-20 19:27:51 +0100

    solving sudoku - in command line

---
+++ Solver.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 18:17:04 2020
+
+@author: PJ
+"""
+
+#https://www.geeksforgeeks.org/sudoku-backtracking-7/
+
+import numpy as np
+
+def find_empty_location(matrix, l): 
+    for row in range(9): 
+        for col in range(9): 
+            if(matrix[row][col]== 0): 
+                l[0]= row 
+                l[1]= col 
+                return True
+    return False
+
+def check_row(matrix, row, num):
+    for i in range(9):
+        if matrix[row][i] == num:
+            return True
+    return False
+
+def check_col(matrix, col, num):
+    for i in range(9):
+        if matrix[i][col] == num:
+            return True
+    return False
+
+def check_box(matrix, row, col, num):
+    for i in range(3):
+        for j in range(3):
+            if matrix[i + row][j + col] == num:
+                return True
+    return False
+
+def is_valid(matrix, col, row, num):
+    return not check_row(matrix,row,num) and not check_col(matrix,col,num) and not check_box(matrix, row - row % 3, col - col % 3, num) 
+
+def solve_sudoku(matrix):
+    l = [0, 0]
+    if not find_empty_location(matrix,l):
+        return True
+    else:
+        row = l[0]
+        col = l[1]
+
+        for num in range(1,10):
+            if is_valid(matrix, col, row, num):
+                matrix[row][col] = num
+                if(solve_sudoku(matrix)):
+                    return True
+                else:
+                    matrix[row][col] = 0
+
+        return False
+
+if __name__ == "__main__":
+    sudoku1 =[[0 for x in range(9)]for y in range(9)] 
+    sudoku1 = [[0,0,0,2,6,0,7,0,1],
+               [6,8,0,0,7,0,0,9,0],
+               [1,9,0,0,0,4,5,0,0],
+               [8,2,0,1,0,0,0,4,0],
+               [0,0,4,6,0,2,9,0,0],
+               [0,5,0,0,0,3,0,2,8],
+               [0,0,9,3,0,0,0,7,4],
+               [0,4,0,0,5,0,0,3,6],
+               [7,0,3,0,1,8,0,0,0]]
+    
+    if solve_sudoku(sudoku1):
+        print(np.array(sudoku1))
+    else:
+        print("No Solution")
--- sudoku.py
+++ sudoku.py
@@ -7,6 +7,28 @@ Created on Sun Sep 20 12:06:14 2020
 
 import cv2
 import numpy as np
+import math
+
+SIZE = 9
+
+def angle_between_two_vectors(vector1, vector2):
+    unit_vector_1 = vector1 / np.linalg.norm(vector1)
+    unit_vector_2 = vector2 / np.linalg.norm(vector2)
+    dot_product = np.dot(unit_vector_1, unit_vector_2)
+    angle = np.arccos(dot_product)
+    return math.degrees(angle)
+
+def check_if_sides_are_equals(A, B, C, D, tolerance):
+    AB = math.sqrt((A[0]-B[0])**2 + (A[1]-B[1])**2)
+    AD = math.sqrt((A[0]-D[0])**2 + (A[1]-D[1])**2)
+    BC = math.sqrt((B[0]-C[0])**2 + (B[1]-C[1])**2)
+    CD = math.sqrt((C[0]-D[0])**2 + (C[1]-D[1])**2)
+    shortest = min(AB, AD, BC, CD)
+    longest = max(AB, AD, BC, CD)
+    return longest > tolerance * shortest
+
+def is_approx_90_degrees(angle, tolerance):
+    return abs(angle - 90) < tolerance
 
 def find_max_contour(contours):
     max_area = -1
@@ -74,6 +96,39 @@ def find_corners_locations(corners):
 
     rect = rect.reshape(4,2)
     return rect
+
+def check_if_is_square(rect):
+    #   A------B
+    #   |      |
+    #   |      |
+    #   D------C
+    A = rect[0]
+    B = rect[1]
+    C = rect[2]
+    D = rect[3]
+    
+    # Calculate vectors
+    AB = B - A
+    AD = D - A
+    BC = C - B
+    DC = C - D
+    
+    # Calculate angles between vectors
+    AB_AD = angle_between_two_vectors(AB, AD)
+    AB_BC = angle_between_two_vectors(AB, BC)
+    BC_DC = angle_between_two_vectors(BC, DC)
+    AD_DC = angle_between_two_vectors(AD, DC)
+    
+    if not (is_approx_90_degrees(AB_AD, 15) and is_approx_90_degrees(AB_BC, 15) and 
+            is_approx_90_degrees(BC_DC, 15) and is_approx_90_degrees(AD_DC, 15)):
+        return False
+    
+    # Check if sides have the same length
+    if check_if_sides_are_equals(A, B, C, D, 1.2):
+        return False
+    
+    return True
+    
         
 cap = cv2.VideoCapture(0)
 
@@ -89,9 +144,51 @@ while True:
         if corners is not None:
             corners = corners.reshape(4,2)
             rect = find_corners_locations(corners)
-            cv2.circle(frame, (rect[2][0], rect[2][1]), 5, (255,0,0), 5)
-        
-    #print(corners[0][0][0])
+            if check_if_is_square(rect):
+                
+                # https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+                (tl, tr, br, bl) = rect
+                bottom_width = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+                top_width = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+                right_height = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+                left_height = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+                
+                max_width = max(int(bottom_width), int(top_width))
+                max_height = max(int(right_height), int(left_height))
+                
+                dst = np.array([
+            		[0, 0],
+            		[max_width - 1, 0],
+            		[max_width - 1, max_height - 1],
+            		[0, max_height - 1]], dtype = "float32")
+                
+                M = cv2.getPerspectiveTransform(rect, dst)
+                warped = cv2.warpPerspective(frame, M, (max_width, max_height))
+                cv2.imshow("Sudoku-Original", warped)
+                warped_copy = warped.copy()
+                warped_copy = cv2.cvtColor(warped_copy, cv2.COLOR_BGR2GRAY)
+                warped_copy = cv2.GaussianBlur(warped_copy, (5,5), 0)
+                warped_copy = cv2.adaptiveThreshold(warped_copy, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2) 
+                warped_copy = cv2.bitwise_not(warped_copy)
+                cv2.imshow("Sudoku-Processed", warped_copy)
+                
+                cell_height = warped_copy.shape[0] // 9
+                cell_width = warped_copy.shape[1] // 9
+                
+                for i in range(SIZE):
+                    for j in range(SIZE):
+                        cell = warped_copy[cell_height*i:cell_height*(i+1), cell_width*j:cell_width*(j+1)]
+                        print(cell.shape)
+                        if cell.shape[0] > 0 and cell.shape[1] > 0:
+                            k = i + j
+                            cv2.imshow(str(k), cell)
+                        
+                        
+              
+            cv2.circle(frame, (rect[0][0], rect[0][1]), 5, (0,0,255), 5)
+            cv2.circle(frame, (rect[1][0], rect[1][1]), 5, (0,0,255), 5)
+            cv2.circle(frame, (rect[2][0], rect[2][1]), 5, (0,0,255), 5)
+            cv2.circle(frame, (rect[3][0], rect[3][1]), 5, (0,0,255), 5)
     
     cv2.drawContours(frame,[max_contour], 0,  (0,255,0), 3)
     cv2.imshow("Original", frame)
